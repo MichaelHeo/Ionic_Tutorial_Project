0abb50df963928ad4c2e07269091a43518141d2e
Michael Heo <michael.kr95@gmail.com>
2017-07-13 19:08:42 -0300

    Class 5 - angular.fromJson, objects, toggle list on card

--- Ionic_Part1/Alura_Car/www/js/controller.js
+++ Ionic_Part1/Alura_Car/www/js/controller.js
@@ -1,10 +1,25 @@
 angular.module('starter').controller('ListagemController', function($scope) {
-    $scope.listaDeCarros = ['BMW 120i', 'ONIX 1.6', 'FIESTA 2.0', 'C3 1.0', 'UNO FIRE', 'SENTRA 2.0',
-                            'ASTRA SEDAN', 'VECTRA 2.0 TURBO', 'HILUX 4X4', 'MONTANA CABINE DUPLA', 'OUTLANDER 2.4', 'FUSCA 1500'];
+    $scope.listaDeCarros = [
+        {"nome": "BMW 120i", "preco": 70000}
+    ]; 
 
 });
 
 angular.module('starter').controller('CarroEscolhidoController', function($stateParams, $scope){
     // stateParams para pegar o parametro passado na rota
-    $scope.carroEscolhido = $stateParams.carro
+    $scope.carroEscolhido = angular.fromJson($stateParams.carro); // Transforma carro em objeto Json
+    $scope.listaDeAcessorios = [
+        {"nome": "Freio ABS", "preco": 800},
+        {"nome": "Ar Condicionado", "preco": 1000},
+        {"nome": "MP3 Player", "preco": 500}
+    ];
+
+    // Funcao pra quando um acessorio for marcado/desmarcado, o preco total mudara!
+    $scope.mudou = function(acessorio, isMarcado){
+        if(isMarcado){
+            $scope.carroEscolhido.preco = $scope.carroEscolhido.preco + acessorio.preco;
+        } else {
+            $scope.carroEscolhido.preco = $scope.carroEscolhido.preco - acessorio.preco;
+        }
+    };
 });
